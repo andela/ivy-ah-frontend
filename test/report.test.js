bd97d041644178a8694f19458fce31d716f0919b
innocentEdosa <ilegbinijieinnocentcareer@gmail.com>
2019-05-27 14:11:17 +0100

    166269508(bug) redirect on social auth

--- test/report.test.js
+++ test/report.test.js
@@ -1,11 +1,12 @@
 import { createStore } from 'redux';
-import articlesReducer from 'Reducers/articlesReducer';
 import * as articleActions from 'Actions/reportArticleAction';
 import * as types from 'Actions/actionTypes';
 import axios from 'axios';
 import configureMockStore from 'redux-mock-store';
 import MockAdapter from 'axios-mock-adapter';
 import thunk from 'redux-thunk';
+import articlesReducer from '../src/reducers/articlesReducer';
+
 
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
