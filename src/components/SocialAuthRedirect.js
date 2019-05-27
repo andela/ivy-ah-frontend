58038b198f5cd781b6e50e013c74776925908262
innocentEdosa <ilegbinijieinnocentcareer@gmail.com>
2019-05-27 15:23:32 +0100

    166269508(bug) redirect on social auth

--- src/components/SocialAuthRedirect.js
+++ src/components/SocialAuthRedirect.js
@@ -1,1 +1,31 @@
-imr
+import React from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import queryString from 'query-string';
+import { decode } from 'jsonwebtoken';
+import { authSuccess } from '../actions/auth';
+
+const SocialAuthRedirect = ({ onAuthSuccess }) => {
+  const value = queryString.parse(location.search);
+  const { token } = value;
+  const user = decode(token) || {};
+  onAuthSuccess(token, user.id, user.email);
+  return (
+    <Redirect to="/" />
+  );
+};
+
+SocialAuthRedirect.propTypes = {
+  onAuthSuccess: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+
+});
+
+const mapDispatchToProps = dispatch => ({
+  onAuthSuccess: (token, userid, email) => dispatch(authSuccess(token, userid, email))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(SocialAuthRedirect);
